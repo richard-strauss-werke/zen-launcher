b054a3d077b5eaf652d8e2d4f81efffe3bc43d33
aerhard <a_erhard@gmx.de>
2014-05-07 17:14:47 +0200

    added regex to extension parameters, extended demo.html

--- src/defaults/preferences/defaults.js
+++ src/defaults/preferences/defaults.js
@@ -1,6 +1,6 @@
 pref("extensions.zenlauncher.event", "runlocaloxygen");
 pref("extensions.zenlauncher.programPath", "C:\\Program files\\Oxygen XML Editor 15\\oxygen15.0.exe");
 pref("extensions.zenlauncher.parameterVariable", "Zen.app.oxyLoadQueue");
-pref("extensions.zenlauncher.parameterRegex", "");
+pref("extensions.zenlauncher.parameterRegex", "^\/db\/(\w|\.(?!\.)|\/)+$");
 pref("extensions.zenlauncher.parameterPrefix", "oxygen:/exist-zen$zen");
 pref("extensions.zenlauncher.parameterSuffix", "");
